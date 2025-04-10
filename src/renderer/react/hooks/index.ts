2afa7aef435d477766109ac5adbd487460d858ef
Kohichi Aoki <drikin@gmail.com>
2025-04-09 17:17:14 -0700

    リファクタリング: 重複するフックレイヤーを削除し、直接コンテキストをエクスポート

--- src/renderer/react/hooks/index.ts
+++ src/renderer/react/hooks/index.ts
@@ -3,11 +3,11 @@
  * 全てのカスタムフックをここからエクスポートすることで、インポートを簡素化します
  */
 
-// コンテキストベースの実装をエクスポート
-export { useTasks } from './useTasks';
-export { useMediaFiles } from './useMediaFiles';
-export { useWaveform } from './useWaveform';
-export { useThumbnail } from './useThumbnail';
+// 直接コンテキストフックをエクスポート（中間レイヤー削除）
+export { useTasks } from '../contexts/TaskContext';
+export { useMedia as useMediaFiles } from '../contexts/MediaContext';
+export { useWaveform } from '../contexts/WaveformContext';
+export { useThumbnail } from '../contexts/ThumbnailContext';
 
 // その他のフック
 // ここに他のフックがあれば追加
